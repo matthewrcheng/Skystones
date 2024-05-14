6e278c5fa141bd3ef69c348a482d97580f275737
mcheng <mcheng@sitscape.com>
2024-05-13 23:44:24 -0400

    - fixed dragging - added images - created card class

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 let r=3, c=3, hand_size=5;
 let initialX, initialY;
 let cardToDrag = null;
+const cars = new Array(r*c).fill(null);
 
 const board = document.getElementById("board");
 const hand = document.getElementById("hand");
+
+class Card {
+    constructor(name, image, up, down, left, right) {
+        this.name = name;
+        this.image = image;
+        this.up = up;
+        this.down = down;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+const apple = new Card("Apple", "img/apple.png", 2, 2, 2, 2);
+const crab = new Card("Crab", "img/crab.png", 2, 2, 2, 2);
+const fish = new Card("Fish", "img/fish.png", 2, 2, 2, 2);
+const gin = new Card("Gin", "img/gin.png", 2, 2, 2, 2);
+const gopher = new Card("Gopher", "img/gopher.png", 2, 2, 2, 2);
+const sheep = new Card("Sheep", "img/sheep.png", 2, 2, 2, 2);
 
 function makeGridCells(row, col) {
     for (let r = 0; r < row; r++) {
@@ -11,6 +30,7 @@ function makeGridCells(row, col) {
         board.appendChild(row).className = "row";
         for (let c = 0; c < col; c++) {
             let cell = document.createElement('div');
+            cell.id = r*3 + c;
             cell.className = "cell";
             row.appendChild(cell);
         }
@@ -26,6 +46,12 @@ function makeHandCards(count) {
             card.classList.add('placed');
             dragMouseDown(e);
         };
+        let img = document.createElement('img');
+        img.src = gopher.image;
+        img.alt = gopher.name;
+        // img.height = '100%';
+        // img.width = 'auto';
+        card.appendChild(img);
         hand.appendChild(card);
     }
 }
@@ -54,18 +80,16 @@ function placeCard(e) {
             cardToDrag.style.transform = "translate(0px, 0px)";
             cardToDrag.onmousedown = null;
             cardToDrag.style.margin = "0";
-            cardToDrag.style.
-            cardToDrag = null;
-            document.onmouseup = null;
-            document.onmousemove = null;
         } else {
             cardToDrag.style.transform = "translate(0px, 0px)";
-            card.classList.remove('placed');
-            cardToDrag = null;
-            document.onmouseup = null;
-            document.onmousemove = null;
+            cardToDrag.classList.remove('placed');
+            
         }
+        let idx = parseInt(cell.id);
     }
+    cardToDrag = null;
+    document.onmouseup = null;
+    document.onmousemove = null;
 }
 
 function getOverlappingCell(e) {
